f7afd44c1feff8fa904e9724f5add4319c870ffb
Luis Angel <siullegnak8@gmail.com>
2017-12-04 19:40:20 -0800

    creating initial commit

---
+++ app/src/main/java/com/hap/baking/BakingActivity.java
@@ -0,0 +1,13 @@
+package com.hap.baking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BakingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baking);
+    }
+}
